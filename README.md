5004b8795416c5716451b75c423375db0e45a41b
Victor Salit <victor.salit@gmail.com>
2014-12-21 20:13:11 +0100

    Final version of the run_analysis and complete Readme. Also renamed the data folder.

--- README.md
+++ README.md
@@ -1,12 +1,92 @@
 Coursera Getting and Cleaning Data course Project
 =================================================
 
+
+### General Info
+
 The goal of this project is to prepare tidy data that can be used for later analysis.
-The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained: 
+The data linked to from the course website represent data collected from the sensors of the Samsung Galaxy S II smartphone.
 
-http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+The experiments have been carried out with a group of 30 volunteers wearing a smartphone on the waist. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING). The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
 
-The dataset, needed for this project, is stored in this repository, so cloning the repo should provide one with both the dataset and the script(s). Should for any reason one need the original one - it can be downloaded from: 
+The sensor signals (accelerometer and gyroscope) were pre-processed by applying noise filters and then sampled in fixed-width sliding windows of 2.56 sec and 50% overlap (128 readings/window). The sensor acceleration signal was separated into Gravity and Body motion components.
+
+In addition to the measured signals, the data set contains many computed variables such as: 
+
+* Jerk - time differentiation of the acceleration and angular velocity (rate of change)
+* Magnitude - Euclidean norm, sqrt(x^2 + y^2 + z^2)
+* Angle between two vectors
+* FFT - conversion into the frequency domain
+
+and many others. A full description is available in the features_info.txt 
+
+The dataset, needed for this project, is stored in this repository, so cloning the repo should provide one with both the dataset and the script. Should for any reason one need the original one - it can be downloaded from: 
 
 https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
 
+### Project description 
+R script called run_analysis.R that does the following: 
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive variable names. 
+5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+### Implementation details
+
+The run_analysis.R is implemented as a function accepting the datafolder name, a logical flag to save the result to the file and a file name (see below in usage). 
+
+The implementation is neither the most efficient nor most elegant. The main point was to make it work within my limited knowledge and experience with the R language at this point. I make use of four packages: data.table, dplyr, tidyr and reshape2. The function tries to load them and if unsuccessful - it will try to install them and then load. If anything goes wrong at this point the corresponding calls to library should make the function fail immediately. See discussion on 
+
+http://yihui.name/en/2014/07/library-vs-require/
+
+The function is very verbose. Almost every action is wrapped into a pair of cat() to give the user an indication of what it does. 
+
+Step 1: The merging is done using a series of rbind with subsequent cbind
+
+Step2: Since there is no a single interpretation of the Step 2 (see the discussions on the course forum), partially owing to the somewhat ambigous naming convention of the data set authors, here is my interpretation:
+
+* 'measurement' corresponds to the direct output of the sensors - accelerometers and gyroscopes (preprocessed or filtered, for the purpose of this exercise).
+* 'Jerks' ,'magnitudes' and 'angles' are computed quantities and as such do not satisfy the requirements of Step 2
+* 'frequencies' are also derived quantities (computed through FFT) and as such do not satisfy the requirements of Step 2 
+* variables from frequency and time domains can not be mixed within the same data set
+
+This leaves us with features having initial 't', mean() or std() and no 'Mag' or 'Jerk' in their names.
+
+Step 3: The activity names are assigned using factor().
+Step 4: In this step we replace the column names with features' names. However to make the subsequent tidying possible the names at this point are slightly changed:
+* the "()" are striped off as well as the 't' prefix
+* hyphens '-' are replaced with periods '.'
+* Periods '.' are inserted where missing
+
+these manipulations result in following name change:
+'tBodyAcc-mean()-X' --> 'Body.Acc.mean.X'
+
+Step 5: This step is basically a melt-separate-spread chain, however I have chosen to compute the required averages a-priory using the aggregate() function, instead of combining this into the spread(). The reason is to be able to cross-check with the values posted on
+https://class.coursera.org/getdata-016/forum/thread?thread_id=50#comment-123
+by Brandon. 
+
+The dataframe represents only two true variables (in the sense of tidy) - mean and standard deviation. The column names are actually combined factors. To tidy this dataframe, it is first melted, so that the column names are now values (rows) stored in a single column, alongside 'subject' and 'activity' columnes. The mean and std values are also molten into a single column. In the next step the column names are separated using the periods as separators and finally the dataframe is spread into its final form of seven columns, of which five are factors: 'subject', 'activity', ' signal.cmpnt', 'sensor', 'axis' and two variables: 'mean', 'std'.
+
+
+### Usage
+1. fork and clone the repo https://github.com/victorsalit/GCDcoursera
+2. 
+'''R
+source("run_analysis.R")
+tidy <- run_analysis(datafolder = "UCI HAR Dataset", save = FALSE, fname = "tidy.txt")
+'''
+
+'''
+Function arguments:
+datafolder  a character string naming the data folder which is assumed to reside in the working directory.    
+save        a logical controlling whether to export the result to a file.
+fname       a character string naming the file to which to write the result in case save=TRUE.
+'''
+
+Note1: to load the saved file directly use the read.table with the option header = TRUE
+'''R
+fromfile <- read.table(file, header = TRUE)
+'''
+Note2: the 'subject' column is a -factor- in the run_analysis output, but it is a -int- in the read.table output.
--- run_analysis.R
+++ run_analysis.R
@@ -1,89 +1,170 @@
-#run_analysis.R
-#
-# R script for tidying the data collected from the accelerometers from the Samsung Galaxy S smartphone
-#
-# Course: Getting and Cleaning Data
-# Author: Victor Salit
-
-
-# Step 0. houskeeping
-# The data folder must reside in the same working directory were this script resides and runs.
-
-rm(list=ls())
-current_dir<-getwd()
-print(paste("current working directory: ", current_dir,sep=" "))
-
-datafolder <- file.path(current_dir,"UCI_HAR_Dataset")
-trainfolder <- file.path(datafolder,"train")
-testfolder <- file.path(datafolder,"test")
+run_analysis <- function(datafolder = "UCI HAR Dataset", save = FALSE, fname = "tidy.txt"){
+    #run_analysis.R
+    #
+    # A function for tidying the data collected from the sensors of the Samsung Galaxy S II smartphone
+    #
+    # Arguments:
+    # datafolder  a character string naming the data folder which is assumed to reside in the working directory.    
+    # save        a logical controlling whether to export the result to a file.
+    # fname       a character string naming the file to which to write the result in case save=TRUE.
+    #
+    # Course: Getting and Cleaning Data
+    # Author: Victor Salit
     
-if(!file.exists(datafolder)){stop("Can't find the folder with data in the working directory.")}
-
-# Step 1. reading and merging the data
-
-# 1.1 Reading:
-print("Found the Dataset folder, trying to read the data")
-
-features   <- read.table(file.path(datafolder,"features.txt"))                      # measurements' labels 
-activities <- read.table(file.path(datafolder,"activity_labels.txt"))             # activites' labels
-
-data_train       <- read.table(file.path(trainfolder,"X_train.txt"))               # training data
-activities_train <- read.table(file.path(trainfolder,"y_train.txt"))         # training activities' IDs
-subjects_train   <- read.table(file.path(trainfolder,"subject_train.txt"))     # subjects' IDs
-
-data_test       <- read.table(file.path(testfolder,"X_test.txt"))
-activities_test <- read.table(file.path(testfolder,"y_test.txt"))
-subjects_test   <- read.table(file.path(testfolder,"subject_test.txt"))
-
-# 1.2 Merging:
-print("reading completed, trying to merge")
-my_data <- rbind(data_train,data_test)
-my_act  <- rbind(activities_train,activities_test)
-my_subj <- rbind(subjects_train,subjects_test)
-
-# 2. extracting the mean and standard deviation
-index_means_std <- grepl("^[t]",features[,2]) & !grepl("Mag",features[,2]) &
-    (grepl("-mean()",features[,2], fixed=TRUE) | grepl("-std()",features[,2], fixed=TRUE))
-meanstd         <- my_data[,index_means_std]
-
-# 3. descriptive activity names
-print("replacing the activity indices with corresponding labels")
-activity <- factor(my_act[,1], labels = activities[,2])
-
-# 4. descriptive variable names
-# 4.1 removing the "BodyBody" typo
-cnames<-gsub("()","",features[index_means_std,2],fixed=TRUE)
-cnames<-gsub("-",".",cnames,fixed=TRUE)
-cnames<-gsub("^t","",cnames)
-cnames<-gsub("Body","Body.",cnames)
-cnames<-gsub("Gravity","Gravity.",cnames)
-cnames<-gsub("Jerk",".Jerk",cnames)
-cnames<-gsub("Body.Acc.Jerk","BodyJerk.Acc",cnames)
-cnames<-gsub("Body.Gyro.Jerk","BodyJerk.Gyro",cnames)
-
-
-# 4.3 replacing the hyphen with period and replacing the column names
-colnames(meanstd)<-cnames
-
-
-
-# 5. tidying
-library(data.table)
-library(dplyr)
-library(tidyr)
-
-# 5.1 complete the dataframe
-merged <- cbind(my_subj,activity,meanstd)
-names(merged)[1] <- "subject"
-
-# 5.2 computing the averages
-meaned <- aggregate(merged[,3:(length(cnames)+2)],by=list(subject=merged$subject,activity=merged$activity),mean)
-meaned<-data.table(meaned)
-setkey(meaned,subject,activity)
-
-# 5.3 melting
-library(reshape2)
-md<-melt(meaned,id=c("subject","activity"))
-tidier <- separate(md,variable,into=c("SignalSource","Measurement","Stats","Axis"),sep = "\\.")
-tidy <- spread(tidier,Stats,value)
-setkey(tidy,subject,activity,SignalSource,Measurement,Axis)
+    # libraries/packages    
+    if (!require(data.table)) {install.packages("data.table")}
+    library(data.table)
+    if (!require(dplyr)) {install.packages("dplyr")}
+    library(dplyr)
+    if (!require(tidyr)) {install.packages("tidyr")}
+    library(tidyr)
+    if (!require(reshape2)) {install.packages("reshape2")}
+    library(reshape2)
+    
+    
+    
+    # Step 0. houskeeping
+    # The data folder must reside in the same working directory were this script resides and runs.
+    
+    current_dir<-getwd()
+    cat(paste("current working directory: ", current_dir,sep=" "),'\n')
+    
+    datafolder  <- file.path(current_dir,datafolder)
+    trainfolder <- file.path(datafolder,"train")
+    testfolder  <- file.path(datafolder,"test")
+    
+    
+    # Step 1. reading and merging the data
+    
+    # 1.1 Reading:
+    if(!file.exists(datafolder)){stop("Can't find the specified data folder in the working directory.")}
+    cat("Found the Data folder\n")
+    if(!file.exists(trainfolder)){stop("Can't find the subfolder with train data in the data folder.")}
+    cat("Found the Train Dataset folder\n")
+    if(!file.exists(testfolder)){stop("Can't find the subfolder with test data in the data folder.")}
+    cat("Found the Test Dataset folder\n")
+    
+    cat('Step 1. Reading and merging the data:\n')
+    
+    cat("features.......... ")
+    features   <- read.table(file.path(datafolder,"features.txt"))                  # measurements' labels 
+    cat("done.\n")
+    
+    cat('activity_labels... ')
+    activities <- read.table(file.path(datafolder,"activity_labels.txt"))           # activites' labels
+    cat("done.\n")
+    
+    cat("x_train........... ")
+    data_train       <- read.table(file.path(trainfolder,"X_train.txt"))            # training data
+    cat("done.\n")
+    
+    cat('y_train........... ')
+    activities_train <- read.table(file.path(trainfolder,"y_train.txt"))            # training activities' IDs
+    cat("done.\n")
+    
+    cat('subject_train..... ')
+    subjects_train   <- read.table(file.path(trainfolder,"subject_train.txt"))      # subjects' IDs
+    cat("done.\n")
+    
+    cat('x_test............ ')
+    data_test       <- read.table(file.path(testfolder,"X_test.txt"))
+    cat("done.\n")
+    
+    cat('y_test............ ')
+    activities_test <- read.table(file.path(testfolder,"y_test.txt"))
+    cat("done.\n")
+    
+    cat('subject_test...... ')
+    subjects_test   <- read.table(file.path(testfolder,"subject_test.txt"))
+    cat("done.\n")
+    
+    # 1.2 Merging:
+    cat("=== Merging:\n")
+    cat('data (x).......... ')
+    my_data <- rbind(data_train,data_test)
+    cat("done.\n")
+    cat('activitites (y)... ')
+    my_act  <- rbind(activities_train,activities_test)
+    cat("done.\n")
+    cat('subjects ids...... ')
+    my_subj <- rbind(subjects_train,subjects_test)
+    cat("done.\n")
+    df <- cbind(my_subj,my_act,my_data)
+    
+    
+    # Step 2. "Extracts only the measurements on the mean and standard deviation for each measurement." 
+    # see Readme for detailed explanations on this step
+    # short: we extract only the time domain quantities (measurements),
+    #        magnitudes, angles and jerks are computed quantities, frequencies too.
+    
+    
+    cat("Step 2. Extracting the measurements on the mean and standard deviation for each measurement... ")
+    index_means_std <- grepl("^[t]", features[,2]) & 
+                      !grepl("Mag", features[,2])  &
+                      !grepl("Jerk", features[,2])  &
+                      (grepl("-mean()", features[,2], fixed=TRUE) | 
+                       grepl("-std()", features[,2], fixed=TRUE))
+    
+    df <- df[,c(1, 2, which(index_means_std) + 2)] # offset of two columnes because of subj & activ
+    cat("done.\n")
+    
+    
+    # Step 3. descriptive activity names
+    cat("Step 3. Replacing the activity indices with corresponding labels... ")
+    df[,2] <- factor(df[,2], labels = activities[,2])
+    cat('done.\n')
+    
+    
+    # 4. descriptive variable names (features)
+    # The column names are stored in the 'features' vector and can be extracted 
+    # using the logical index vector computed in the step 2.
+    # These names, however, must be slightly corrected not only because of style 
+    # considerations, but to make the tidying easier later on. 
+    #
+    # In case one would like to tidy the frequency domain data, there might be a 
+    # need to first remove the "BodyBody" typo with: 
+    # cnames <- gsub("BodyBody", "Body", features[index_means_std,2], fixed=TRUE)
+    
+    cat("Step 4. Replacing the column names... ")
+    cnames <- gsub("()", "", features[index_means_std,2], fixed=TRUE)  # remove "()"
+    cnames <- gsub("-", ".", cnames, fixed=TRUE)                       # replace "-" with "."
+    cnames <- gsub("^t", "", cnames)                                   # remove initial "t"
+    cnames <- gsub("Body", "Body.", cnames)                            # insert "." after "Body"
+    cnames <- gsub("Gravity", "Gravity.", cnames)                      # insert "." after "Gravity" 
+    
+    colnames(df) <- c("subject","activity",cnames)
+    cat("done.\n")
+    
+    
+    # Step 5. tidying
+    
+    cat("Step 5. Tidying the data... ")
+    
+    # computing the averages:
+    # it is possible to include this step later into spreading the molten dataset,
+    # however for debugging reasons it was beneficial to perform this step before 
+    # final reshaping. The values in 'averaged' correspond to values posted on
+    # https://class.coursera.org/getdata-016/forum/thread?thread_id=50#comment-123
+    # by Brandon. 
+    averaged <- aggregate(df[,3:(length(cnames)+2)],by=list(subject=df$subject,activity=df$activity),mean)
+    averaged <- data.table(averaged)
+    
+    # tidying
+    melted <- melt(averaged,id=c("subject","activity"))
+    tidier <- separate(melted,variable,into=c("signal.cmpnt","sensor","stats","axis"),sep = "\\.")
+    tidy <- spread(tidier,stats,value)
+    
+    # converting the non-variables to factors:
+    tidy$subject <- formatC(tidy$subject, width=2, flag="0") 
+    tidy <- data.table(data.frame(unclass(tidy), stringsAsFactors = TRUE))
+    setkey(tidy,subject,activity,signal.cmpnt,sensor,axis) # reordering
+    cat("done.\n")
+    
+    # 6 Output
+    if(save) {
+        write.table(tidy,file=fname,row.names=FALSE)
+        cat('The Data hase been written to ',fname,'\n')
+    }
+    cat("Analysis completed.\n")
+    tidy
+}
